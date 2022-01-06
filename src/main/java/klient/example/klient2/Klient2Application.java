d2b6c3fa165dadd765287812221c12adce4c69c8
AlexUaNo <aleksandra.chornous@gmail.com>
2022-01-06 12:08:36 +0100

    Initial commit

---
+++ src/main/java/klient/example/klient2/Klient2Application.java
@@ -0,0 +1,13 @@
+package klient.example.klient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Klient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Klient2Application.class, args);
+    }
+
+}
